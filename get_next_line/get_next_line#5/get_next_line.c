50dc0f3cda95750e26e49b8763c62587af70896b
Mael Gallais <mgallais@c2r12p2.42lehavre.fr>
2023-11-10 15:52:27 +0100

    Ahoy~ !

---
+++ get_next_line/get_next_line#5/get_next_line.c
@@ -0,0 +1,142 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgallais <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 09:20:04 by mgallais          #+#    #+#             */
+/*   Updated: 2023/11/10 15:40:03 by mgallais         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_extract_line(char *stash)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!stash[i])
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	if (stash[i] == '\n')
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+static char	*ft_read_fd(int fd, char *stash)
+{
+	char	*buff;
+	int		bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(stash, '\n') && bytes != 0)
+	{
+		bytes = read(fd, buff, BUFFER_SIZE);
+		if (bytes == -1 || !buff)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[bytes] = '\0';
+		stash = ft_strjoin(stash, buff);
+	}
+	free(buff);
+	return (stash);
+}
+
+char	*ft_clean_stash(char *stash)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+	{
+		free(stash);
+		return (NULL);
+	}
+	if (ft_strlen(stash) - i + 1 > 0)
+		str = malloc(ft_strlen(stash) - i + 1);
+	else
+		str = malloc(1);
+	if (!str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (stash[i])
+		str[j++] = stash[i++];
+	str[j] = '\0';
+	free(stash);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*buf;
+	static char	*stash;
+
+	buf = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash = ft_read_fd(fd, stash);
+	if (!stash)
+		return (NULL);
+	buf = ft_extract_line(stash);
+	stash = ft_clean_stash(stash);
+	return (buf);
+}
+// debug
+/*
+int	main(void)
+{
+	char	*line;
+	int		i;
+	int		fd1;
+	int		fd2;
+	int		fd3;
+	fd1 = open("tests/test.txt", O_RDONLY);
+	fd2 = open("tests/test2.txt", O_RDONLY);
+	fd3 = open("tests/test3.txt", O_RDONLY);
+	i = 1;
+	while (i < 7)
+	{
+		line = get_next_line(fd1);
+		printf("line [%02d]: %s\n", i, line);
+		free(line);
+		line = get_next_line(fd2);
+		printf("line [%02d]: %s\n", i, line);
+		free(line);
+		line = get_next_line(fd3);
+		printf("line [%02d]: %s\n", i, line);
+		free(line);
+		i++;
+	}
+	close(fd1);
+	close(fd2);
+	close(fd3);
+	return (0);
+}
+*/
---
+++ get_next_line/get_next_line#5/get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgallais <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 09:19:59 by mgallais          #+#    #+#             */
+/*   Updated: 2023/11/10 15:46:24 by mgallais         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+// get_next_line.c
+char	*get_next_line(int fd);
+
+// get_next_line_utils.c (libft)
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+size_t	ft_strlen(char *s);
+
+#endif
