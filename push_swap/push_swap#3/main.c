e4bb5ff03a345f108e61e6d0512f831dceae99d5
Mael Gallais <mgallais@c2r12p4.42lehavre.fr>
2023-11-20 15:40:23 +0100

    Ahoy~ !

---
+++ push_swap/push_swap#3/main.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgallais <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 11:39:44 by mgallais          #+#    #+#             */
+/*   Updated: 2023/11/20 15:15:27 by mgallais         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pushswap.h"
+
+void	error_exit(void)
+{
+	ft_putstr_fd("Error\n", 2);
+	exit(EXIT_FAILURE);
+}
+
+static void	error_handle(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	if (argc == 1)
+		error_exit();
+	while (argv[j])
+	{
+		while (argv[j][i])
+		{
+			if (argv[j][i] != '-' && (argv[j][i] > '9' || argv[j][i] < '0'))
+				error_exit();
+			i++;
+		}
+		i = 0;
+		j++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	error_handle(argc, argv);
+	core_ft(argv + 1);
+	return (EXIT_SUCCESS);
+}
