ab44559e3efac63062dd609636783ab3cbc5c9e0
Mael Gallais <mgallais@c2r12p2.42lehavre.fr>
2023-12-22 10:00:20 +0100

    Ahoy~ !

---
+++ minitalk/minitalk#4/client.c
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgallais <mgallais@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 09:51:21 by mgallais          #+#    #+#             */
+/*   Updated: 2023/12/22 09:57:02 by mgallais         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./include/minitalk.h"
+
+static int	check_argument(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc == 3)
+	{
+		while (argv[1][i])
+		{
+			if (!ft_isdigit(argv[1][i]))
+				return (0);
+			i++;
+		}
+		i = 0;
+		while (argv[2][i])
+			i++;
+		return (i + 1);
+	}
+	return (0);
+}
+
+static void	send_size(unsigned int size, pid_t pid)
+{
+	int	i;
+
+	i = 31;
+	ft_printf("Size = ");
+	while (i >= 0)
+	{
+		if ((size >> i) & 1)
+		{
+			ft_printf("1");
+			kill(pid, SIGUSR2);
+		}
+		else
+		{
+			ft_printf("0");
+			kill(pid, SIGUSR1);
+		}
+		i--;
+		usleep(SLEEP);
+	}
+	ft_printf(" = %d\n", size);
+}
+
+static void	send_str(const char *str, pid_t pid)
+{
+	int	i;
+
+	i = 42;
+	while (*(str - 1) || i == 42)
+	{
+		i = 7;
+		ft_printf("Letter = ");
+		while (i >= 0)
+		{
+			if ((*str >> i) & 1)
+			{
+				ft_printf("1");
+				kill(pid, SIGUSR2);
+			}
+			else
+			{
+				ft_printf("0");
+				kill(pid, SIGUSR1);
+			}
+			i--;
+			usleep(SLEEP);
+		}
+		ft_printf(" = %c\n", *str);
+		str++;
+	}
+}
+
+static void	client_handler(int signum)
+{
+	if (signum == SIGUSR1)
+		ft_printf(" ---> Successfully wrote on the server, Yippiiiie !!!\n");
+	else if (signum == SIGUSR2)
+		ft_printf(" ---> Error : Server could not print the message ;(\n");
+	else if (signum == SIGINT)
+	{
+		ft_printf(" ---> Error : Manual exit\n");
+		exit(0);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	unsigned int	size;
+	pid_t			pid;
+
+	size = check_argument(argc, argv);
+	if (size)
+	{
+		pid = ft_atoi(argv[1]);
+		send_str(ft_itoa(getpid()), pid);
+		send_size(size, pid);
+		send_str(argv[2], pid);
+		signal(SIGUSR1, client_handler);
+		signal(SIGUSR2, client_handler);
+		signal(SIGINT, client_handler);
+		ft_printf("\nWaiting for server response...\n");
+		while (42)
+			pause();
+	}
+	else
+		ft_printf("Correct syntax : ./client <SERVER_PID> <TEXT>\n");
+	return (0);
+}
