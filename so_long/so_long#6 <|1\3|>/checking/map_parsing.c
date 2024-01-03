cd6a3faeae5af4b6beee66ba40ca9afd4d777e09
Mael Gallais <mgallais@c1r6p7.42lehavre.fr>
2024-01-03 13:46:08 +0100

    Ahoy~ !

---
+++ so_long/so_long#6 <|1\3|>/checking/map_parsing.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_parsing.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgallais <mgallais@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 16:28:12 by mgallais          #+#    #+#             */
+/*   Updated: 2024/01/03 13:06:47 by mgallais         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long.h"
+
+static void	create_map(t_data *data, char *filepath)
+{
+	int		y;
+	int		x;
+	int		fd;
+	char	*buf;
+
+	y = 0;
+	x = 0;
+	fd = open(filepath, O_RDONLY);
+	while (y != data->size_y)
+	{
+		buf = get_next_line(fd);
+		while (buf[x] != '\n')
+		{
+			data->map[y][x] = buf[x];
+			data->check_map[y][x] = '0';
+			x++;
+		}
+		x = 0;
+		y++;
+	}
+	close(fd);
+	ft_printf("|\tMap created\n");
+	y = 0;
+	while (y != data->size_y)
+		ft_printf("|\t\t%s\n", data->map[y++]);
+}
+
+void	map_parsing(t_data *data, char *filepath)
+{
+	int		i;
+	int		fd;
+	char	*buf;
+
+	i = 0;
+	ft_printf("|  Map parsing :\n");
+	fd = open(filepath, O_RDONLY);
+	buf = get_next_line(fd);
+	data->size_x = ft_strlen(buf) - 1;
+	while (buf)
+	{
+		(data->size_y)++;
+		buf = get_next_line(fd);
+	}
+	close(fd);
+	data->map = malloc(sizeof(char *) * data->size_y);
+	data->check_map = malloc(sizeof(char *) * data->size_y);
+	while (i != data->size_y)
+	{
+		data->map[i] = malloc(sizeof(char) * data->size_x);
+		data->check_map[i++] = malloc(sizeof(char) * data->size_x);
+	}
+	create_map(data, filepath);
+}
---
+++ so_long/so_long#6 <|1\3|>/include/so_long.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgallais <mgallais@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 15:05:47 by mgallais          #+#    #+#             */
+/*   Updated: 2024/01/03 13:39:10 by mgallais         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <fcntl.h>
+# include <MLX42/MLX42.h>
+# include "libft.h"
+
+typedef struct s_data
+{
+	char		**map;
+	char		**check_map;
+	int			cur_x;
+	int			cur_y;
+	int			size_x;
+	int			size_y;
+	int			start_amount;
+	int			collectible_i;
+	int			collectible_amount;
+	int			exit_i;
+	int			exit_amount;
+}	t_data;
+
+
+void	error_exit(char	*strerror);
+
+// ber_checking
+void	ber_check(int argc, char **argv, t_data *data);
+void	map_parsing(t_data *data, char *filepath);
+void	doublon_error(t_data *data);
+void	check_rectangle(t_data *data);
+void	check_walls(t_data *data);
+int		check_is_possible(t_data *data, int y, int x);
+
+
+#endif
