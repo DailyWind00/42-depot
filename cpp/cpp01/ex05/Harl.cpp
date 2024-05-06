fa79e13ae489190f8c30862f1877fba44191b6aa
Mael Gallais <mgallais@c2r12p2.42lehavre.fr>
2024-05-06 15:50:24 +0200

    Ahoy~ !

--- cpp/cpp01/ex05/Harl.cpp
+++ cpp/cpp01/ex05/Harl.cpp
@@ -6,7 +6,29 @@
 /*   By: mgallais <mgallais@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 15:38:33 by mgallais          #+#    #+#             */
-/*   Updated: 2024/05/06 15:38:34 by mgallais         ###   ########.fr       */
+/*   Updated: 2024/05/06 15:50:15 by mgallais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+
+void	debug( void ){
+	std::cout <<  "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
+}
+
+void	info( void ){
+	std::cout <<  "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void	warning( void ){
+	std::cout <<  "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	error( void ){
+	std::cout <<  "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
